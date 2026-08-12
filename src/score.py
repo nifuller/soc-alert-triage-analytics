477a9f905c5e5ee7f3a042699635fbde9d16276b
Nick <nifuller@protonmail.com>
2026-08-12 08:45:01 -0700

    created read csv function to obtain the alerts.csv file

--- src/score.py
+++ src/score.py
@@ -1,6 +1,11 @@
-import rules
 import pandas as pd
 from clean import clean_data
+
+RULES_TARGETS = {
+    "brutef_force_rate": ["FTP-Patator", "SSH-Patator"],
+    "dos_flood": ["DoS Hulk", "DoS GoldenEye"],
+    "dos_low_andslow": ["DoS slowloris", "Dos Slowhttptest"],
+}
 
 def get_orig_labels():
     """Gets the orignal labels from the clean dataset 
@@ -16,8 +21,28 @@ def get_orig_labels():
     
     return monday_df, tuesday_df, wednesday_df
 
+def read_csv(path="alerts.csv"):
+    """Obtains the alerts.csv file and stores it as a pandas
+    data frame
+
+    Args:
+        path (str, optional):The name of the csv file. Defaults to "alerts.csv".
+
+    Returns:
+        Dataframe: Returns a pandas dataframe containing all the 
+        alerts.
+    """
+    alerts_df = pd.read_csv(path)
+    
+    # #sanity check to ensure it pulls alerts.csv correctly
+    # print(alerts_df["Label"].value_counts())
+    
+    return alerts_df
+
+
 def main():
-    get_orig_labels()
+    # get_orig_labels()
+    # read_csv()
 
 if __name__ == "__main__":
     main()
