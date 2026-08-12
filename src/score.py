4018aa07883748f20d3cee21b9d971ef2f14da6b
Nick <nifuller@protonmail.com>
2026-08-12 08:29:14 -0700

    created get original labels function

--- src/score.py
+++ src/score.py
@@ -1,7 +1,23 @@
+import rules
+import pandas as pd
+from clean import clean_data
 
+def get_orig_labels():
+    """Gets the orignal labels from the clean dataset 
+
+    Returns:
+        Dataframe: Returns 3 dataframes: monday_df, tuesday_df, & wednesday_df
+    """
+    monday_df, tuesday_df, wednesday_df = clean_data()
+    
+    # #sanity check
+    # print('Label' in wednesday_df.columns)
+    # print(wednesday_df['Label'].value_counts())
+    
+    return monday_df, tuesday_df, wednesday_df
 
 def main():
-    pass
+    get_orig_labels()
 
 if __name__ == "__main__":
     main()
