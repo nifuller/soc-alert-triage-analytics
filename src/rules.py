66a718a1e2923b0fa1ded0df367f04cddab0ae6f
Nick <nifuller@protonmail.com>
2026-08-14 07:36:12 -0700

    re ran project to get tuned results

--- src/rules.py
+++ src/rules.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from clean import clean_data
 
-#pre tuned values 
+
 AUTH_PORTS = {21, 22}
-MAX_FWD_PACKES = 8 
 ATTEMPT_PER_MIN = 20
+## pre tuned values 
+# MAX_FWD_PACKES = 8 
+
 
 #tuned values
-# MAX_FWD_PACKES = 20
+MAX_FWD_PACKES = 20
 
 
 HTTP_PORT = 80
