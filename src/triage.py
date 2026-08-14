9115f10c2c4e7148b47b7f0b7db531f528f03d42
Nick <nifuller@protonmail.com>
2026-08-14 09:31:32 -0700

    created rank rules by noise and alert vol over time functions

--- src/triage.py
+++ src/triage.py
@@ -20,6 +20,23 @@ def load_alert(path="alerts.csv"):
     
     return alerts_df
 
+def load_results(results_path="results.csv", attacks_results_path="results_per_attack.csv"):
+    """_summary_
+
+    Args:
+        results_path (str, optional): _description_. Defaults to "results.csv".
+        attacks_results_path (str, optional): _description_. Defaults to "results_per_attack.csv".
+
+    Returns:
+        _type_: _description_
+    """
+    
+    results_df = pd.read_csv(results_path)
+    results_per_attack_df = pd.read_csv(attacks_results_path)
+    
+    return results_df, results_per_attack_df
+    
+    
 def alert_vol_per_rule(alerts_df):
     """_summary_
 
@@ -33,14 +50,46 @@ def alert_vol_per_rule(alerts_df):
     
     print(number_of_alerts)
         
+def rank_rules_by_noise(results_df):
+    rules_sorted_by_FP = results_df.sort_values(by=["FP"], ascending=False)
+    # print(rules_sorted_by_FP)
     
+    return rules_sorted_by_FP
+       
+def alert_vol_over_time(alerts_df):
+    """_summary_
+
+    Args:
+        alerts_df (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     
+    alerts_df['Timestamp'] = pd.to_datetime(alerts_df['Timestamp'])
+    alerts_df_sorted = alerts_df.sort_values(by=['Timestamp', 'rule'])
+    
+    hourly_alerts_count = (
+        alerts_df_sorted.set_index('Timestamp')
+        .groupby(['rule'])
+        .resample('h')
+        .size()
+        .reset_index(name='count')
+        .sort_values(by=['rule', 'Timestamp', 'count'], ascending=[True, True, False])
+    )
+    
+    # print(hourly_alerts_count)
+    
+    return hourly_alerts_count
 
 
 
 def main():
     alerts_df = load_alert()
     alert_vol_per_rule(alerts_df)
+    results_df, _ = load_results()
+    rank_rules_by_noise(results_df)
+    alert_vol_over_time(alerts_df)
 
 if __name__ == '__main__':
     main()
